28fe853313d85cba8992cbc6449b82c0bdd5c513
somecallmejosh <josh@thebrileys.com>
2025-04-17 11:34:18 -0400

    Update content on about and contact pages.

--- pages/about.vue
+++ pages/about.vue
@@ -14,22 +14,73 @@ const goodFit = [
   'Experience in <strong>implementing and maintaining design systems</strong> for large-scale applications.',
   'Experience <strong>integrating front-end interfaces with backend REST APIs</strong>.',
 ]
+
+const resumeItems = [
+ {
+    title: 'Senior UI Developer',
+    company: 'Logatot, Inc.',
+    date: '2023 – 2024',
+    description: 'Developed and maintained modular, accessible UI components using Ruby on Rails ViewComponents, ensuring design consistency and responsive layouts.'
+  },
+  {
+    title: 'Senior Software Engineer',
+    company: "America's Test Kitchen",
+    date: '2022 – 2023',
+    description: 'Built and documented UI components with React, Next.js, and Storybook, streamlining feature delivery across multiple projects.'
+  },
+  {
+    title: 'Principal Design Engineer',
+    company: 'Berkshire Hathaway Specialty Insurance',
+    date: '2018 – 2022',
+    description: 'Created a custom UI component library within Vue.js/Nuxt.js, standardizing front-end development and accelerating feature rollouts.'
+  },
+  {
+    title: 'Lead UI Developer',
+    company: "America's Test Kitchen",
+    date: '2016 – 2018',
+    description: 'Designed and implemented a scalable UI utility system, reducing development overhead and improving team velocity.'
+  },
+  {
+    title: 'UI Designer & Front End Developer',
+    company: 'Sports Technologies, Inc.',
+    date: '2013 - 2016',
+    description: 'Lead Designer and front-end developer for Ruby on Rails and Angular (6.x) web applications commissioned by elite sporting organizations such as NFL, NASCAR, NHL, MLS, PGA, and more.'
+  },
+  {
+    title: 'Freelance UI Developer',
+    company: 'Self-Employed',
+    date: '2010 - Present',
+    description: 'Worked with various clients to develop and maintain web applications, focusing on performance optimization and accessibility.'
+  },
+  {
+    title: 'Web Designer & Front End Developer',
+    company: 'ImageWorks, LLC',
+    date: '2009 - 2013',
+    description: 'Front end developer and web designer for a variety of clients, including the Aetna Insurance.'
+  },
+  {
+    title: 'Owner, Web Designer and Front End Developer',
+    company: 'Para-Diddle Design, LLC',
+    date: '2005 - 2009',
+    description: 'Responsible for vision, budgeting, marketing/sales, and other administrative tasks.'
+  },
+]
 </script>
 
 <template>
   <div class="space-y-12">
     <h1 class="text-5xl">
-      About Me
-    </h1>
-    <div class="grid lg:grid-cols-2 gap-12 lg:gap-24">
+        About Me
+      </h1>
+    <div class="grid 2xl:grid-cols-2 gap-12 2xl:gap-24 lg:items-center">
       <div class="prose">
         <AnimateImage
           src="https://res.cloudinary.com/dwjulenau/image/upload/ar_3:2,c_fill,dpr_auto,f_auto,fl_progressive,q_auto/v1744317106/josh-portfolio/assets_task_01jrgnzqzhe1w9d68qj5z60crx_img_0.webp"
           alt="UX Engineer presenting to business people"
           :scaleY="0.75"
           />
-        <h2 class="text-balance">Enterprise UX Engineering & Front-End Development</h2>
-
+      </div>
+      <div class="prose">
         <p>
           With 20 years of experience in front-end development, <em>
           I specialize in architecting and implementing scalable,
@@ -45,15 +96,23 @@ const goodFit = [
           implementing rigorous testing strategies, and ensuring adherence to accessibility
           standards (WCAG 2.1 or higher).
         </p>
-
-      </div>
-      <div class="prose">
-        <h2>Key areas of expertise</h2>
-        <ul class="not-prose space-y-2 divide-y-1 divide-neutral-100">
-          <li v-for="(item, index) in goodFit"
-            class="py-1 text-pretty" v-html="item" />
-        </ul>
       </div>
     </div>
+    <ul class="not-prose space-y-6 grid md:grid-cols-2 2xl:grid-cols-3 gap-12">
+      <li v-for="(item, index) in resumeItems" :key="index">
+        <small class="inline-block mb-4 text-xs px-1.5 bg-neutral-900 text-white font-semibold tracking-wide rounded-full">{{ item.date }}</small>
+        <div class="prose">
+          <h3 class="text-balance text-xl">
+            {{ item.title }}
+          </h3>
+          <p class="mb-2 font-semibold">
+            {{ item.company }}
+          </p>
+          <p>
+            {{ item.description }}
+          </p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -9,31 +9,38 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="prose">
-    <h1 class="font-[400] flex items-center gap-1">
-      Contact Me
-    </h1>
+  <div>
+    <div class="grid lg:grid-cols-2 gap-6 lg:gap-24">
+      <div class="prose order-2 lg:order-1">
+        <small class="inline-block mb-4 text-xs px-1.5 bg-neutral-900 text-white font-semibold tracking-wide rounded-full">Contact Me</small>
+        <h1>Enterprise UX Engineering & Consulting</h1>
+        <p>
+          <strong>For organizations seeking an experienced UX engineer to lead front-end development</strong>, provide
+          expert consultation on
+          UI architecture, or implement scalable and accessible component libraries, I offer specialized services tailored
+          to enterprise requirements. I collaborate with teams that prioritize clear communication, efficient execution, and
+          measurable outcomes.
+        </p>
+      </div>
+      <div class="order-1 lg:order-2">
+        <AnimateImage
+          src="https://res.cloudinary.com/dwjulenau/image/upload/ar_3:2,c_fill,dpr_auto,f_auto,fl_progressive,q_auto/v1744317571/josh-portfolio/assets_task_01jrgpq4pdefgrxbb6xbx72yyw_img_0.webp"
+          class="w-full h-auto mb-4 rounded-lg border border-neutral-100 grayscale"
+          alt="UX Engineer presenting to business people"
+          :scaleY="0.75"
+        />
+      </div>
+    </div>
 
-    <h2>Enterprise UX Engineering & Consulting</h2>
-    <AnimateImage
-      src="https://res.cloudinary.com/dwjulenau/image/upload/ar_3:2,c_fill,dpr_auto,f_auto,fl_progressive,q_auto/v1744317571/josh-portfolio/assets_task_01jrgpq4pdefgrxbb6xbx72yyw_img_0.webp"
-      class="w-full h-auto mb-4 rounded-lg border border-neutral-100 grayscale"
-      alt="UX Engineer presenting to business people"
-      :scaleY="0.75"
-    />
-    <p>
-      <strong>For organizations seeking an experienced UX engineer to lead front-end development</strong>, provide
-      expert consultation on
-      UI architecture, or implement scalable and accessible component libraries, I offer specialized services tailored
-      to enterprise requirements. I collaborate with teams that prioritize clear communication, efficient execution, and
-      measurable outcomes.
-    </p>
 
-    <h2>How to Reach Me</h2>
-    <ul class="not-prose">
-      <li class="flex items-center gap-1"><Icon name="skill-icons:gcp-dark" /> josh@thebrileys.com</li>
-      <li class="flex items-center gap-1"><Icon name="skill-icons:linkedin" />/somecallmejosh</li>
-      <li class="flex items-center gap-1"><Icon name="skill-icons:twitter" />/joshuabriley</li>
-    </ul>
+
+    <div class="space-y-4">
+      <h2 class="text-2xl">How to Reach Me</h2>
+      <ul class="not-prose space-y-4">
+        <li class="flex items-center gap-1"><Icon name="skill-icons:gcp-dark" /> josh@thebrileys.com</li>
+        <li class="flex items-center gap-1"><Icon name="skill-icons:linkedin" />/somecallmejosh</li>
+        <li class="flex items-center gap-1"><Icon name="skill-icons:twitter" />/joshuabriley</li>
+      </ul>
+    </div>
   </div>
 </template>
