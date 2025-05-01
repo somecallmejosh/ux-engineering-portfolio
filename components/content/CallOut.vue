4f5468ca002a37a34ae7c272217efabfaf322787
somecallmejosh <josh@thebrileys.com>
2025-05-01 10:48:28 -0400

    Content style updates.

--- components/content/CallOut.vue
+++ components/content/CallOut.vue
@@ -1,3 +1,3 @@
 <template>
-  <div class="not-prose bg-blue-50 p-4 rounded-lg"><slot /></div>
+  <div class="not-prose bg-blue-50 p-4 rounded-lg text-blue-950 border-l-4 border-blue-100 text-sm"><slot /></div>
 </template>
